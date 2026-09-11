1a7bab90436add1aa30d291f7ed9c30d74185cc3
Sherik Fayzulin <sherikf1@gmail.com>
2026-09-11 18:15:40 +0500

    Update server.mjs

--- server.mjs
+++ server.mjs
@@ -4,7 +4,7 @@ import axios from 'axios';
 const app = express();
 app.use(express.json());
 
-const TELEGRAM_BOT_TOKEN = '8174290649:AAHWsd3npj68BbWYN9-SEfJnJl98x_myzf4';
+const TELEGRAM_BOT_TOKEN = '8174290649:AAE19TrudUvJCw-6t4_s-kgo288xCOgvXRk';
 const TELEGRAM_CHAT_ID = '-1002360533502'; // ✅ ВЕРНО
 
 app.all('/jivo-webhook', async (req, res) => {
